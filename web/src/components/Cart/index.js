e627d68612fa1362b89bda62e7f41e938ec8c928
Filipe Pinheiro <filippinheiro1@gmail.com>
2020-01-27 02:20:58 +0000

    integrando checkout do pagar.me

--- web/src/components/Cart/index.js
+++ web/src/components/Cart/index.js
@@ -1,7 +1,9 @@
-import React from 'react';
+import React from 'react'
+import api from '../../services/api'
 import './styles.css'
+import config from '../../config/private.json'
 
-function LoginForm({ quantity, amount }) {
+function Cart({ quantity, amount }) {
 
    function format(value) {
       const price = value / 100
@@ -9,8 +11,52 @@ function LoginForm({ quantity, amount }) {
       return price_formatted
    }
 
+   async function handleSubmit(event) {
+       event.preventDefault()
+       const checkout = new window.PagarMeCheckout.Checkout({
+        encryption_key: config.API_KEY,
+        success: function(data) {
+            api.post('/capture', {
+                token,
+                recipient_id: 're_ck5v9pxgj09l1xg6fs3e3zsva'
+            })
+            console.log(data);
+        },
+        error: function(err) {
+        	console.log(err);
+        },
+        close: function() {
+        	console.log('The modal has been closed.');
+        }
+      });
+
+      const token = await checkout.open({
+        amount,
+        buttonText: 'Pagar',
+        buttonClass: 'botao-pagamento',
+        customerData: 'false',
+        createToken: 'true',
+        paymentMethods: 'credit_card',
+        customer: {
+          external_id: '#123456789',
+          name: 'Fulano',
+          type: 'individual',
+          country: 'br',
+          email: 'fulano@email.com',
+          documents: [
+            {
+              type: 'cpf',
+              number: '71404665560',
+            },
+          ],
+          phone_numbers: ['+5511999998888', '+5511888889999'],
+          birthday: '1985-01-01'
+        }
+      });
+   }
+
    return (
-      <form>
+      <form onSubmit={handleSubmit}>
          <label>{quantity} Itens no carrinho</label>
          <label>{format(amount)} - valor total</label>
          <button type="submit">Finalizar Compra</button>
@@ -18,4 +64,4 @@ function LoginForm({ quantity, amount }) {
    )
 }
 
-export default LoginForm;
+export default Cart;
