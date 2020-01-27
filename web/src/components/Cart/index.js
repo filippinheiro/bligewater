be54f3bcb23b2d241aa6d0425418a3f957d4be96
Filipe Pinheiro <filippinheiro1@gmail.com>
2020-01-27 02:46:54 +0000

    feature fix : terminal checkout funcional

--- web/src/components/Cart/index.js
+++ web/src/components/Cart/index.js
@@ -17,7 +17,7 @@ function Cart({ quantity, amount }) {
         encryption_key: config.API_KEY,
         success: function(data) {
             api.post('/capture', {
-                token,
+                id: data.token,
                 recipient_id: 're_ck5v9pxgj09l1xg6fs3e3zsva'
             })
             console.log(data);
@@ -30,29 +30,55 @@ function Cart({ quantity, amount }) {
         }
       });
 
-      const token = await checkout.open({
-        amount,
-        buttonText: 'Pagar',
-        buttonClass: 'botao-pagamento',
-        customerData: 'false',
-        createToken: 'true',
-        paymentMethods: 'credit_card',
-        customer: {
-          external_id: '#123456789',
-          name: 'Fulano',
-          type: 'individual',
-          country: 'br',
-          email: 'fulano@email.com',
-          documents: [
-            {
-              type: 'cpf',
-              number: '71404665560',
-            },
-          ],
-          phone_numbers: ['+5511999998888', '+5511888889999'],
-          birthday: '1985-01-01'
-        }
-      });
+    checkout.open({
+    amount,
+    createToken: 'true',
+    paymentMethods: 'credit_card',
+    customerData: false,
+    billing: {
+      name: 'Ciclano de Tal',
+      address: {
+        country: 'br',
+        state: 'SP',
+        city: 'São Paulo',
+        neighborhood: 'Fulanos bairro',
+        street: 'Rua dos fulanos',
+        street_number: '123',
+        zipcode: '05170060'
+      }
+    },
+    shipping: {
+      name: 'Ciclano de Tal',
+      fee: 12345,
+      delivery_date: '2017-12-25',
+      expedited: true,
+      address: {
+        country: 'br',
+        state: 'SP',
+        city: 'São Paulo',
+        neighborhood: 'Fulanos bairro',
+        street: 'Rua dos fulanos',
+        street_number: '123',
+        zipcode: '05170060'
+      }
+    },
+    items: [
+      {
+        id: '1',
+        title: 'Bola de futebol',
+        unit_price: 12000,
+        quantity: 1,
+        tangible: true
+      },
+      {
+        id: 'a123',
+        title: 'Caderno do Goku',
+        unit_price: 3200,
+        quantity: 3,
+        tangible: true
+      }
+    ]
+  })
    }
 
    return (
